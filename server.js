1428017b06d60e12ba902d2ab6176b51d0995974
Fletzz <felipbents@gmail.com>
2025-06-03 00:08:34 -0300

    colisão-parede

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
+const GRID_SIZE = 40; // 800/20 = 40 cells
 
 app.use(express.static('public'));
 
@@ -11,10 +12,18 @@ let players = {};
 
 io.on('connection', (socket) => {
   console.log('Jogador conectado:', socket.id);
-  players[socket.id] = { x: 15, y: 15, direction: 'right' };
+  players[socket.id] = { x: 20, y: 20, direction: 'right', isAlive: true };
 
   socket.on('move', (direction) => {
-    if (players[socket.id]) players[socket.id].direction = direction;
+    if (players[socket.id] && players[socket.id].isAlive) {
+      players[socket.id].direction = direction;
+    }
+  });
+
+  socket.on('restart', () => {
+    if (players[socket.id]) {
+      players[socket.id] = { x: 20, y: 20, direction: 'right', isAlive: true };
+    }
   });
 
   socket.on('disconnect', () => {
@@ -25,11 +34,24 @@ io.on('connection', (socket) => {
 setInterval(() => {
   for (let id in players) {
     let p = players[id];
+    if (!p.isAlive) continue;
+
+    let newX = p.x;
+    let newY = p.y;
+
     switch (p.direction) {
-      case 'up': p.y -= 1; break;
-      case 'down': p.y += 1; break;
-      case 'left': p.x -= 1; break;
-      case 'right': p.x += 1; break;
+      case 'up': newY -= 1; break;
+      case 'down': newY += 1; break;
+      case 'left': newX -= 1; break;
+      case 'right': newX += 1; break;
+    }
+
+    // Verifica colisão com as paredes
+    if (newX < 0 || newX >= GRID_SIZE || newY < 0 || newY >= GRID_SIZE) {
+      p.isAlive = false;
+    } else {
+      p.x = newX;
+      p.y = newY;
     }
   }
   io.emit('state', players);
